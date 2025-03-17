739acf0239f6d68040b4a2019e212c27a8ebd36c
Kythonlk <109597262+kythonlk@users.noreply.github.com>
2025-03-17 11:21:44 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,13 +5,13 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install snapd
 
 echo "🔹 Installing APT packages..."
-sudo apt install -y 
-    chrome-gnome-shell \
+sudo apt install -y chrome-gnome-shell \
     tmux \
+    fzf \
+    ripgrep \
+    xclip \
     vlc \
-    remmina \
     btop \
-    golang \
     gcc \
     openjdk-19-jdk \
     fonts-hack \
@@ -25,6 +25,18 @@ echo "🔹 Installing Snap packages..."
 sudo snap install android-studio --classic
 sudo snap install nvim --classic
 sudo snap install beekeeper-studio --classic
+sudo snap install go --classic
+
+echo "🔹 Installing Chrome..."
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm ./google-chrome-stable_current_amd64.deb
+
+echo "🔹 Installing Brave..."
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
 
 echo "🔹 Installing Anydesk..."
 curl -fsSL https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo gpg --dearmor -o /usr/share/keyrings/anydesk-archive-keyring.gpg
@@ -44,7 +56,7 @@ echo "🔹 Installing GNOME Extensions..."
 flatpak install -y flathub org.gnome.Extensions
 gnome-extensions enable blur-my-shell@aunetx \
     && gnome-extensions enable clipboard-indicator@tudmotu.com \
-    && gnome-extensions enable resource-monitor@oryon
+    && gnome-extensions enable Resource_Monitor@Ory0n
 
 echo "🔹 Adding user to Docker group..."
 sudo usermod -aG docker $USER
